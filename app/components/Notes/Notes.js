10c3c40689b9305f595d193d31b56137e179988f
tigershen23 <tigershen23@gmail.com>
2015-10-06 07:09:31 -0700

    Add PropTypes validation to Repos, UserProfile, Notes

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -5,6 +5,10 @@
 var React = require('react');
 
 var Notes = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    notes: React.PropTypes.array.isRequired
+  },
 
   render: function() {
     return (
