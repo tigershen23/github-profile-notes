4f2ebbed29735e485997b1efb04187705fb6cf14
tigershen23 <tigershen23@gmail.com>
2015-10-05 20:36:29 -0700

    Add routing via React Router

---
+++ app/App.js
@@ -0,0 +1,11 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./config/routes');
+
+Router.run(routes, function(Root) {
+  React.render(<Root />, document.getElementById('app'));
+});
---
+++ app/components/Home.js
@@ -0,0 +1,19 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+
+var Home = React.createClass({
+
+  render: function() {
+    return (
+      <h2 className="text-center">
+        Search by GitHub Username Above
+      </h2>
+    );
+  }
+
+});
+
+module.exports = Home;
