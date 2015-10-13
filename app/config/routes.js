3d908182a852ec540bf4342ed6c0a8493da957ce
tigershen23 <tigershen23@gmail.com>
2015-10-13 08:42:02 -0700

    Update routes, helpers, and App to use ES6 features

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,8 @@
-var React = require('react');
-var Main = require('../components/Main');
-var Home = require('../components/Home');
-var Profile = require('../components/Profile');
-var Router = require('react-router');
-var DefaultRoute = Router.DefaultRoute;
-var Route = Router.Route
+import React from "react"
+import Main from "../components/Main"
+import Home from "../components/Home"
+import Profile from "../components/Profile"
+import { Router, Route, DefaultRoute } from "react-router"
 
 module.exports = (
   <Route name="app" path="/" handler={Main}>
