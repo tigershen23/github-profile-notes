03fd5fe71e252e8a2f31ed3564bde5c8bbe10368
tigershen23 <tigershen23@gmail.com>
2015-10-24 13:10:24 -0700

    Add passing structure test for SearchGithub

--- app/components/__tests__/SearchGithub-test.js
+++ app/components/__tests__/SearchGithub-test.js
@@ -1,36 +1,31 @@
 import React from "react"
 import TestUtils from "react-addons-test-utils"
 let expect = require('expect');
-let stubRouterContext = require("../../utils/stubRouterContext")
 
 let SearchGithub = require('../SearchGithub');
 
 describe("root", function() {
-  let Subject = stubRouterContext(SearchGithub, {}, {})
   beforeEach(function() {
     this.TestUtils = TestUtils
     this.renderer = TestUtils.createRenderer()
     this.renderer.render(<SearchGithub />, { router: React.PropTypes.func })
+    this.result = this.renderer.getRenderOutput()
   })
 
   it("renders without error", function() {
-    let result = this.renderer.getRenderOutput()
-    expect(result).toExist()
+    expect(this.result).toExist()
   })
 
-  it("renders as a div", function() {
-    let result = this.renderer.getRenderOutput()
+  it("renders with the correct structure", function() {
+    expect(this.result.type).toEqual("div")
 
     //debugger;
-    expect(result.type).toEqual("div")
-    expect(result.props.children)
-    //<form onSubmit={this.handleSubmit.bind(this)}>
-      //<div className="form-group col-sm-7">
-        //<input className="form-control" type="text" ref="username" />
-      //</div>
-      //<div className="form-group col-sm-5">
-        //<button type="submit" className="btn btn-block btn-primary">Search Github</button>
-      //</div>
-    //</form>
+    let usernameInput = this.result.props.children.props.children[0].props.children
+    expect(usernameInput.type).toEqual("input")
+    expect(usernameInput.ref).toEqual("username")
+
+    let submitButton = this.result.props.children.props.children[1].props.children
+    expect(submitButton.type).toEqual("button")
+    expect(submitButton.props.children).toEqual("Search Github")
   });
 })
