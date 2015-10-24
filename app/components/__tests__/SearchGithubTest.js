070a6b9861f633a5945dfd4d5dff9cea44c45c70
tigershen23 <tigershen23@gmail.com>
2015-10-24 07:48:27 -0700

    Get better package versions (e.g. React 0.14) and write an actual test

--- app/components/__tests__/SearchGithubTest.js
+++ app/components/__tests__/SearchGithubTest.js
@@ -1,21 +1,21 @@
-import React from "react/addons"
+import React from "react"
+import TestUtils from "react-addons-test-utils"
 let expect = require('expect');
 let stubRouterContext = require("../../utils/stubRouterContext")
 
 let SearchGithub = require('../SearchGithub');
-let { TestUtils } = React.addons
 
 describe("root", function() {
   let Subject = stubRouterContext(SearchGithub, {}, {})
   beforeEach(function() {
-
     this.TestUtils = TestUtils
     this.renderer = TestUtils.createRenderer()
-    this.renderer.render(<SearchGithub />)
-    this.renderOutput = this.renderer.getRenderOutput()
+    this.renderer.render(<SearchGithub />, { router: React.PropTypes.func })
   })
 
   it("renders without problems", function() {
-    expect(this.renderOutput).toExist()
+    let result = this.renderer.getRenderOutput()
+    expect(result).toExist()
+    expect(result.type).toEqual("div")
   });
 })
