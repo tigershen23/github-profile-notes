c0ed61c4b4cd5fa2c371ed944bb00720896452c4
tigershen23 <tigershen23@gmail.com>
2015-11-06 17:48:59 -0800

    Add Home-test.js

---
+++ app/components/__tests__/Home-test.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TestUtils from "react-addons-test-utils"
+import shallowRender from "../../utils/test/shallowRender"
+import ShallowTestUtils from "react-shallow-testutils"
+
+let { Simulate, renderIntoDocument } = TestUtils
+let { findWithType } = ShallowTestUtils
+let Home = require('../Home');
+
+describe("Home", function() {
+  describe("structure", function() {
+    beforeEach(function() {
+      this.result = shallowRender(<Home />)
+    })
+
+    it("renders an h2 with search prompt", function() {
+      let text = this.result.props.children
+
+      expect(this.result.type).toEqual("h2")
+      expect(text).toMatch("Search by GitHub Username")
+    })
+  })
+})
